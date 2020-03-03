8b7978bd45b81c2d03b9653f355275025e3c61b2
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-03-02 21:53:24 -0800

    Initial Commit

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+//Handle registering users, adding users, etc.
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/users
+// @desc    Test route
+// @access  Public
+router.get('/', (req, res) => res.send('User route'));
+
+module.exports = router;
