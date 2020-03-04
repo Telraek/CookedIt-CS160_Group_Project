f8e8892b896656acb818f02dab4101b36880fa9f
Benjamin Yaun <Telraek@gmail.com>
2020-03-03 19:56:49 -0800

    Directory Structure creation and MasterBranch cleanup
    Cleaned up master branch. 3 top-level directories for later organization created.

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS160
-Group Project for CookedIt in CS160
--- routes/api/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
-
-module.exports = router;
