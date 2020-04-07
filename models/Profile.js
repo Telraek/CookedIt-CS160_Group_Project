2808c0e3986415f47e1e8b87dbc78cfd2699f180
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-04-06 23:02:13 -0700

    Started working on setting up Profile model and API routes.

---
+++ models/Profile.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  //Want to create a reference to user model, because every profile should be associated with a User.
+  user: {
+    //Connect to a ID in the User model -> new User has _id field.
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    tpye: String,
+  },
+  //How experienced they are with cooking.
+  experience: {
+    type: String,
+    required: true,
+  },
+  //There favorite recipes to cook/learn about.
+  recipes: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  //An object with all their social media links.
+  social: {
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagrams: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,50 @@
 //Handle routes that have anything to do with profiles. (Fetching them, adding them, updating them, etc)
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me (Just our profile based on User ID that is in the token.)
+// @desc    Get current users profile
+// @access  Private
+
+//The auth in the second parameter indicates that you want this to be a protected route.
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name']);
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user.' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update a user profile
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      //Makes sure that these fields have values since they are currently marked as required.
+      check('experience', 'Experience is required.').not().isEmpty(),
+      check('recipes', 'Recipes is required.').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {}
+);
 
 module.exports = router;
